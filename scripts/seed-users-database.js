46e245de5b8d09184fdc7af30ce87e91d29ce25c
amygoldberg <amykgoldberg@gmail.com>
2015-06-24 14:36:32 -0400

    adding additional seed user data

--- scripts/seed-users-database.js
+++ scripts/seed-users-database.js
@@ -4,55 +4,79 @@ mongoose.connect('mongodb://localhost/nozama');
 
 var User = require('../lib/users.js');
 
-
 var removeUsers = function(done) {
-      User.remove({}, done);
-    };
+  User.remove({}, done);
+};
 
 var createUserOne = function(done) {
   User.create({
-  firstName: 'John',
-  lastName: 'Doe',
-  phoneNumber: '617-123-1234',
-  address: [{
-          addressType: 'mailing',
-          street: '100 Somerville Ave',
-          city: 'Somerville',
-          state: 'MA',
-          zipCode: '01200',
-          country: 'United States of America'
-        }, {
-          addressType: 'billing',
-          street: '20 Glenwood Park',
-          city: 'Cambridge',
-          state: 'MA',
-          zipCode: '02124'
-        }],
-  emailAddress: 'john@example.com'
+    firstName: 'John',
+    lastName: 'Doe',
+    phoneNumber: '617-123-1234',
+    address: [{
+      addressType: 'mailing',
+      street: '100 Somerville Ave',
+      city: 'Somerville',
+      state: 'MA',
+      zipCode: '01200',
+      country: 'United States of America'
+    }, {
+      addressType: 'billing',
+      street: '20 Glenwood Park',
+      city: 'Cambridge',
+      state: 'MA',
+      zipCode: '02124'
+    }],
+    emailAddress: 'john@example.com'
   }, done);
 };
 
 var createUserTwo = function(done) {
   User.create({
-  firstName: 'Jeanne',
-  lastName: 'Dean',
-  phoneNumber: '617-001-1234',
-  address: [{
-          addressType: 'mailing',
-          street: '100 Central street',
-          city: 'Boston',
-          state: 'MA',
-          zipCode: '01201',
-          country: 'United States of America'
-        }],
-  emailAddress: 'jeanne@example.com'
+    firstName: 'Jeanne',
+    lastName: 'Dean',
+    phoneNumber: '617-001-1234',
+    address: [{
+      addressType: 'mailing',
+      street: '100 Central street',
+      city: 'Boston',
+      state: 'MA',
+      zipCode: '01201',
+      country: 'United States of America'
+    }],
+    emailAddress: 'jeanne@example.com'
+  }, done);
+};
+
+var createUserThree = function(done) {
+  User.create({
+    firstName: 'Kylie',
+    lastName: 'Quackenbush',
+    phoneNumber: '845-555-6186',
+    address: [{
+      addressType: 'billing'
+      street: '13 Rocklyn Drive',
+      city: 'Suffern',
+      state: 'NY',
+      zipCode: '10901',
+      country: 'USA'
+    }, {
+      addressType: 'mailing'
+      street: '13 Rocklyn Drive',
+      city: 'Suffern',
+      state: 'NY',
+      zipCode: '10901',
+      country: 'USA'
+    }],
+    emailAddress: 'kyliequackenbush@gmail.com',
   }, done);
 };
 
 async.series([
     removeUsers,
     createUserOne,
-    createUserTwo
+    createUserTwo,
+    createUserThree
   ],
   function(error) {
     if (error) {
